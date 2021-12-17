67c84487da3a34ee47bf0da26c049317a2df8357
Hiago <hiagotnt1@gmail.com>
2021-12-17 12:17:27 -0300

    init prisma

--- src/orm/prisma.ts
+++ src/orm/prisma.ts
@@ -1,14 +1,20 @@
+import { OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { CreateLogDto } from '../interfaces/log.interface';
 import { Orm } from '../interfaces/orm.interface';
 
-export class PrismaHandler implements Orm {
+export class PrismaHandler extends PrismaClient implements Orm, OnModuleInit {
   connectionName: string;
   tableName: string;
 
   constructor(connectionName: string, tableName: string) {
+    super();
     this.connectionName = connectionName;
     this.tableName = tableName;
+  }
+
+  async onModuleInit() {
+    await this.$connect();
   }
 
   async logExceptionHandler(data: CreateLogDto): Promise<void> {
