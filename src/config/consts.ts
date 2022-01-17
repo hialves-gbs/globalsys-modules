fc1fdcf8ba5c8ff217e2bcc85f2beb8b0548eac5
Hiago <hiagotnt1@gmail.com>
2022-01-17 16:00:24 -0300

    add tenants config

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -5,3 +5,45 @@ export const integrationOrms = {
   typeorm: TypeormHandler,
   prisma: PrismaHandler,
 };
+
+export const tenantsConfig = {
+  branches: {
+    tableName: 'branches',
+  },
+  clients: {
+    tableName: 'clients',
+  },
+  clientPhones: {
+    tableName: 'client_phones',
+  },
+  products: {
+    tableName: 'products',
+  },
+  orders: {
+    tableName: 'orders',
+  },
+  sellers: {
+    tableName: 'sellers',
+  },
+  griffe: {
+    tableName: 'grife',
+  },
+  saleDetail: {
+    tableName: 'sale_detail',
+  },
+  customersMetrics: {
+    tableName: 'customers_metrics',
+  },
+  segmentation: {
+    tableName: 'segmentation',
+  },
+  segmentationConfigs: {
+    tableName: 'segmentation_configs',
+  },
+  groupProductsClients: {
+    tableName: 'group_products_clients',
+  },
+  appointmentsMenu: {
+    tableName: 'appointments_menu',
+  },
+};
